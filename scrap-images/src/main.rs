564baa87f6f6f448b7838e9670e0d565e4afe944
jRimbault <jacques.rimbault@gmail.com>
2022-04-04 11:13:30 +0200

    cosmetics

--- scrap-images/src/main.rs
+++ scrap-images/src/main.rs
@@ -1,39 +1,56 @@
 use clap::Parser;
 use select::document::Document;
 use select::predicate::Name;
-use tokio_stream::{self as stream, StreamExt};
+use std::path::{Path, PathBuf};
 
-#[derive(Parser)]
+#[derive(Parser, Clone)]
 #[clap(author, version)]
 struct Cli {
     url: reqwest::Url,
     #[clap(parse(from_os_str), default_value = "out")]
-    out_dir: std::path::PathBuf,
+    out_dir: PathBuf,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = Cli::parse();
+    let Cli { url, out_dir } = Cli::parse();
     let document = {
-        let res = reqwest::get(args.url.clone()).await?.text().await?;
+        let res = reqwest::get(url.clone()).await?.text().await?;
         Document::from(res.as_str())
     };
     let images_urls = document
         .find(Name("img"))
         .filter_map(|node| node.attr("src"))
-        .filter_map(|image_source| image_url(&args.url, image_source).ok());
-    tokio::fs::create_dir_all(&args.out_dir).await?;
-    let mut iter = stream::iter(images_urls).map(|url| async { download_image(&args, url).await });
-    while let Some(r) = iter.next().await {
-        println!("{}", r.await?);
+        .filter_map(|image_source| image_url(&url, image_source));
+    tokio::fs::create_dir_all(&out_dir).await?;
+
+    let mut receiver = {
+        let (sender, receiver) = tokio::sync::mpsc::channel(32);
+        for url in images_urls {
+            let sender = sender.clone();
+            let out_dir = out_dir.clone();
+            tokio::spawn(async move {
+                sender
+                    .send(download_image(&out_dir, url).await)
+                    .await
+                    .unwrap();
+            });
+        }
+        receiver
+    };
+    while let Some(result) = receiver.recv().await {
+        match result {
+            Ok(path) => println!("{}", path),
+            Err(error) => eprintln!("{}", error),
+        }
     }
     Ok(())
 }
 
-async fn download_image(args: &Cli, url: reqwest::Url) -> anyhow::Result<String> {
+async fn download_image(out_dir: &Path, url: reqwest::Url) -> anyhow::Result<String> {
     let image = reqwest::get(url.clone()).await?;
     let file = url.path_segments().unwrap().last().unwrap();
-    let file = args.out_dir.join(file);
+    let file = out_dir.join(file);
     let mut file = tokio::fs::File::create(file).await?;
     let bytes = image.bytes().await?;
     let mut bytes = bytes.as_ref();
@@ -41,14 +58,14 @@ async fn download_image(args: &Cli, url: reqwest::Url) -> anyhow::Result<String>
     Ok(url.path().to_owned())
 }
 
-fn image_url(base_url: &reqwest::Url, image_source: &str) -> anyhow::Result<reqwest::Url> {
+fn image_url(base_url: &reqwest::Url, image_source: &str) -> Option<reqwest::Url> {
     let r1 = reqwest::Url::parse(image_source);
     if let Ok(url) = r1 {
-        return Ok(url);
+        return Some(url);
     }
     let r2 = base_url.join(image_source);
     if let Ok(url) = r2 {
-        return Ok(url);
+        return Some(url);
     }
-    anyhow::bail!("{} and {}", r1.unwrap_err(), r2.unwrap_err())
+    None
 }
