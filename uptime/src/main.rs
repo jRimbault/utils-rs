90b55ab1ceda5024479f9f964d86f724c2819b6b
jRimbault <jacques.rimbault@gmail.com>
2022-04-18 01:26:13 +0200

    uptime

--- uptime/src/main.rs
+++ uptime/src/main.rs
@@ -25,70 +25,76 @@ struct Args {
 
 #[derive(Parser, Debug)]
 struct ArgsTimings {
-    /// in seconds
-    #[clap(short, long, default_value_t = 10)]
+    /// in milliseconds
+    #[clap(short, long, default_value_t = 10_000)]
     interval: u64,
     /// in seconds
     #[clap(short, long, default_value_t = 24 * 60 * 60)]
     period: u64,
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Timings {
+    interval: Duration,
+    period: Duration,
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let (interval, timeout) = args.timings.to_duration();
+    let timings = args.timings.to_duration();
     let mut map = IndexMap::new();
     loop {
         let start = chrono::Utc::now();
-        let uptime = crossbeam::scope(|scope| {
+        let stats = crossbeam::scope(|scope| {
             let (sender, receiver) = crossbeam::channel::bounded(0);
             scope.spawn(move |_| {
                 for (i, uptime) in receiver.into_iter().enumerate() {
-                    eprint!("{i:<6}{uptime:.2}%\r");
+                    eprint!("{:<6}{uptime:.2}%\r", i + 1);
                     io::stderr().flush().unwrap();
                 }
-                eprintln!();
+                eprint!("             \r");
+                io::stderr().flush().unwrap();
             });
-            poll_for(
-                (args.ip_address, args.port.get()),
-                interval,
-                timeout,
-                sender,
-            )
+            poll((args.ip_address, args.port.get()), timings, sender)
         })
-        .unwrap();
-        map.insert(start, uptime?);
-        println!("{map:#?}");
+        .unwrap()?;
+        println!(
+            "{start}: {stats:>10?} [{}/{} tests]",
+            stats.successes(),
+            stats.len()
+        );
+        map.insert(start, stats);
     }
 }
 
-fn poll_for<A>(
+fn poll<A>(
     address: A,
-    interval: Duration,
-    timeout: Duration,
+    timings: Timings,
     sender: crossbeam::channel::Sender<Percent>,
-) -> anyhow::Result<Percent>
+) -> anyhow::Result<Stats>
 where
     A: ToSocketAddrs,
     A: Clone,
 {
     let start = Instant::now();
+    let mut list = BitVec::new();
+    let address = address.to_socket_addrs()?.next().unwrap();
     loop {
-        let mut list = BitVec::new();
-        match TcpStream::connect(address.clone()) {
+        if start.elapsed() >= timings.period {
+            return Ok(Stats(list));
+        }
+        match TcpStream::connect_timeout(&address, timings.interval / 2) {
             Ok(_) => list.push(true),
             Err(_) => list.push(false),
         }
         let uptime = uptime_rate(&list)?;
         sender.send(uptime).unwrap();
-        if start.elapsed() >= timeout {
-            return Ok(uptime);
-        }
-        thread::sleep(interval);
+        thread::sleep(timings.interval);
     }
 }
 
 fn uptime_rate(list: &BitVec) -> Result<Percent, conv::GeneralErrorKind> {
-    let successes = f64::value_from(list.iter_ones().count())?;
+    let successes = f64::value_from(list.count_ones())?;
     let total = f64::value_from(list.len())?;
     Ok(Percent(successes / total))
 }
@@ -97,22 +103,47 @@ fn uptime_rate(list: &BitVec) -> Result<Percent, conv::GeneralErrorKind> {
 #[derive(Clone, Copy)]
 struct Percent(f64);
 
-impl std::fmt::Display for Percent {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        (self.0 * 100.).fmt(f)
+struct Stats(BitVec);
+
+impl Stats {
+    fn uptime_rate(&self) -> Result<Percent, conv::GeneralErrorKind> {
+        uptime_rate(&self.0)
     }
-}
-impl std::fmt::Debug for Percent {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
+    fn successes(&self) -> usize {
+        self.0.count_ones()
+    }
+    fn len(&self) -> usize {
+        self.0.len()
     }
 }
 
 impl ArgsTimings {
-    fn to_duration(&self) -> (Duration, Duration) {
-        (
-            Duration::from_secs(self.interval),
-            Duration::from_secs(self.period),
-        )
+    fn to_duration(&self) -> Timings {
+        Timings {
+            interval: Duration::from_millis(self.interval),
+            period: Duration::from_secs(self.period),
+        }
+    }
+}
+
+mod wrappers {
+    use super::{Percent, Stats};
+
+    impl std::fmt::Display for Percent {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            (self.0 * 100.).fmt(f)
+        }
+    }
+    impl std::fmt::Debug for Percent {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            self.0.fmt(f)
+        }
+    }
+
+    impl std::fmt::Debug for Stats {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let rate = self.uptime_rate().map_err(|_| std::fmt::Error)?;
+            write!(f, "{:.2}%", rate)
+        }
     }
 }
