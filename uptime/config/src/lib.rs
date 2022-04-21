dda3a8d31a7479f59065f50eb2e1394bd70759f5
jRimbault <jacques.rimbault@gmail.com>
2022-04-21 12:07:11 +0200

    uptime

--- uptime/config/src/lib.rs
+++ uptime/config/src/lib.rs
@@ -1,5 +1,9 @@
 use serde::Deserialize;
-use std::{collections::HashMap, net::IpAddr, num::NonZeroU16, time::Duration};
+use std::{
+    collections::HashMap,
+    net::{IpAddr, SocketAddr},
+    time::Duration,
+};
 
 #[derive(Debug, Deserialize, Clone)]
 #[serde(untagged)]
@@ -10,65 +14,18 @@ pub enum Host {
 
 #[serde_with::serde_as]
 #[derive(Debug, Deserialize, Clone)]
-struct ServerSerde {
-    host: Host,
-    #[serde(skip_serializing_if = "Vec::is_empty")]
-    ports: Vec<NonZeroU16>,
+pub struct Server {
+    pub servers: Vec<SocketAddr>,
     #[serde_as(as = "Option<serde_with::DurationMilliSeconds<u64>>")]
     #[serde(default)]
-    interval: Option<Duration>,
+    pub interval: Option<Duration>,
     #[serde_as(as = "Option<serde_with::DurationSeconds<u64>>")]
     #[serde(default)]
-    period: Option<Duration>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Server {
-    pub host: Host,
-    pub ports: Vec<u16>,
-    pub interval: Option<Duration>,
     pub period: Option<Duration>,
 }
 
 #[derive(Debug, Deserialize, Clone)]
-pub struct ConfigSerde {
-    #[serde(flatten)]
-    servers: HashMap<String, ServerSerde>,
-}
-
-#[derive(Debug, Clone)]
 pub struct Config {
+    #[serde(flatten)]
     pub servers: HashMap<String, Server>,
-}
-
-impl Server {
-    fn from_ser(server: &ServerSerde) -> Server {
-        let ServerSerde {
-            host,
-            ports,
-            interval,
-            period,
-        } = server;
-        Server {
-            host: host.clone(),
-            ports: ports.iter().map(|p| p.get()).collect(),
-            interval: *interval,
-            period: *period,
-        }
-    }
-}
-
-impl ConfigSerde {
-    pub fn server(&self, name: &str) -> Option<Server> {
-        self.servers.get(name).map(Server::from_ser)
-    }
-    pub fn usable(self) -> Config {
-        let ConfigSerde { servers } = self;
-        Config {
-            servers: servers
-                .into_iter()
-                .map(|(name, server)| (name, Server::from_ser(&server)))
-                .collect(),
-        }
-    }
 }
