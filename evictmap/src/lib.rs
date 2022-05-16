07669d0ecd83c22fcf6e79a82eeba3b1d8400ba0
jRimbault <jacques.rimbault@gmail.com>
2022-05-16 19:45:10 +0200

    evictmap

--- evictmap/src/lib.rs
+++ evictmap/src/lib.rs
@@ -8,40 +8,46 @@ pub struct EvictMap<K> {
     map: HashMap<K, bucket::Bucket>,
 }
 
-#[derive(Debug, Clone)]
-pub struct Node<T> {
-    value: T,
-    number: usize,
-}
-
 impl<K> EvictMap<K>
 where
     K: Hash + Eq,
 {
-    pub fn add(&mut self, value: K) -> Node<K>
+    pub fn add(&mut self, value: K) -> usize
     where
         K: Clone,
     {
-        let number = self.map.entry(value.clone()).or_default().add_one();
-        Node { value, number }
+        self.map.entry(value.clone()).or_default().add_one()
     }
 
-    pub fn remove<Q>(&mut self, value: &Q, number: usize) -> Option<()>
+    pub fn remove<Q>(&mut self, value: &Q, number: usize) -> bool
     where
         Q: ?Sized,
         K: Borrow<Q>,
         Q: Hash + Eq,
     {
-        self.map.get_mut(value)?.remove(number)
+        self.map
+            .get_mut(value)
+            .and_then(|bucket| bucket.remove(number))
+            .is_some()
     }
 }
 
-impl<T> std::fmt::Display for Node<T>
-where
-    T: std::fmt::Display,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}{}", self.value, self.number)?;
-        Ok(())
+#[cfg(test)]
+mod test {
+    use super::EvictMap;
+
+    #[test]
+    fn scenario_1() {
+        let mut map = EvictMap::default();
+        assert_eq!(map.add("apibox"), 0);
+        assert_eq!(map.add("apibox"), 1);
+        assert_eq!(map.add("apibox"), 2);
+        assert_eq!(map.add("sitebox"), 0);
+        assert!(!map.remove("apibox", 3));
+        assert!(map.remove("apibox", 1)); // remove 1 first
+        assert!(map.remove("apibox", 0)); // then 0
+        assert_eq!(map.add("apibox"), 0); // get back 0 first
+        assert_eq!(map.add("apibox"), 1); // then 1
+        println!("{map:#?}");
     }
 }
