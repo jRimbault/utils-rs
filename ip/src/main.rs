3e5047b2e9076b12660e5f472d03eda4e3f1f3f5
jRimbault <jacques.rimbault@gmail.com>
2022-07-20 20:14:26 +0200

    use color-eyre

--- ip/src/main.rs
+++ ip/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Context;
+use color_eyre::{eyre::WrapErr, Help, SectionExt};
 use std::net::IpAddr;
 
 /// This program helps getting your WAN address
@@ -13,25 +13,29 @@ enum Ip {
     V6,
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() -> color_eyre::eyre::Result<()> {
+    color_eyre::install()?;
     let ip: Ip = clap::Parser::parse();
-    let address = ip.get().context("getting your IP address")?;
+    let address = ip.get().wrap_err("getting your IP address")?;
     println!("{address}");
     Ok(())
 }
 
 impl Ip {
-    fn get(&self) -> anyhow::Result<IpAddr> {
+    fn get(&self) -> color_eyre::eyre::Result<IpAddr> {
         let url = match self {
             Ip::V4 => "https://api.ipify.org/?format=text",
             Ip::V6 => "https://api64.ipify.org/?format=text",
         };
-        Ok(ureq::get(url)
-            .call()
-            .context("calling the ipify API")?
+        let response = ureq::get(url).call().wrap_err("calling the ipify API")?;
+        let response = response
             .into_string()
-            .context("converting ipify response into an UTF-8 string")?
+            .wrap_err("converting ipify response into an UTF-8 string")?;
+        let ip = response
             .trim()
-            .parse()?)
+            .parse()
+            .wrap_err("parsing response")
+            .section(response.header("ipify response body"))?;
+        Ok(ip)
     }
 }
