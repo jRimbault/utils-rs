220e99ba9dcf06d7713cb806b4f39fde3a787cf7
jRimbault <jacques.rimbault@gmail.com>
2022-07-25 14:35:16 +0200

    add comments

--- unc/src/main.rs
+++ unc/src/main.rs
@@ -1,9 +1,23 @@
-use std::path::{Path, PathBuf};
+/// This program is useless on any platform but Windows
+///
+/// https://superuser.com/questions/29933/get-the-current-unc-path-from-a-local-path-in-powershell
+///
+/// The standard library `canonicalize` function returns the correct UNC path on Windows.
 
-// https://superuser.com/questions/29933/get-the-current-unc-path-from-a-local-path-in-powershell
+use std::{
+    env,
+    io,
+    path::{Path, PathBuf},
+};
 
+#[cfg(not(windows))]
+fn main() -> Result<(), &'static str> {
+    Err("this is useless")
+}
+
+#[cfg(windows)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = match (std::env::args_os().nth(1), std::env::current_dir()) {
+    let path = match (env::args_os().nth(1), env::current_dir()) {
         (Some(path), _) => canonicalize(path),
         (None, Ok(path)) => canonicalize(path),
         _ => Err("no path given")?,
@@ -12,6 +26,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn canonicalize<P: AsRef<Path>>(path: P) -> std::io::Result<PathBuf> {
+fn canonicalize<P: AsRef<Path>>(path: P) -> io::Result<PathBuf> {
     path.as_ref().canonicalize()
 }
