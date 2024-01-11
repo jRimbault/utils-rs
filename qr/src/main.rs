e1f5f0bb5d6ba1bb4fc9597e16fbd53daa6e913b
jRimbault <jacques.rimbault@gmail.com>
2024-01-11 17:33:27 +0100

    qr code maker

--- qr/src/main.rs
+++ qr/src/main.rs
@@ -1,53 +1,85 @@
 use std::{
     fs::File,
-    io::{BufReader, Read},
     path::{Path, PathBuf},
 };
 
 use anyhow::Context;
 use clap::Parser;
-use qrcode::QrCode;
+use qriter::qriter;
 
 #[derive(Debug, Parser)]
 struct Args {
-    file: Option<PathBuf>,
+    file: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    if let Some(file_path) = args.file {
-        let file = File::open(&file_path)?;
-        let out = file_path.file_stem().unwrap();
-        std::fs::create_dir_all(out).context("mkdir out")?;
-        qr(file, out.as_ref())?;
-    } else {
-        std::fs::create_dir_all("out").context("mkdir out")?;
-        qr(std::io::stdin().lock(), "out".as_ref())?;
+    let file = File::open(&args.file)?;
+    let name = args
+        .file
+        .file_stem()
+        .context("file name should have a stem")?;
+    let out = Path::new(name);
+    std::fs::create_dir_all(out)?;
+    for (i, image) in qriter(file).enumerate() {
+        let name = format!("{:02}-qrcode.png", i + 1);
+        image
+            .save(out.join(&name))
+            .context(format!("writing {name:?}"))?
     }
     Ok(())
 }
 
-fn qr<R>(reader: R, out: &Path) -> anyhow::Result<()>
-where
-    R: Read,
-{
-    let mut reader = BufReader::new(reader);
-    let mut buffer = [0u8; 2048];
-    let mut i = 1;
-    loop {
-        let len = match reader.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(len) => len,
-            Err(e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
-            Err(e) => return Err(e)?,
-        };
-        let code = QrCode::new(&buffer[..len])?;
-        let image = code.render::<image::Luma<u8>>().build();
-        let name = format!("{:02}-qrcode.png", i);
-        image
-            .save(out.join(&name))
-            .context(format!("writing {name:?}"))?;
-        i += 1;
+mod qriter {
+    use image::ImageBuffer;
+    use image::Luma;
+    use qrcode::QrCode;
+    use std;
+    use std::io::BufReader;
+    use std::io::Read;
+
+    pub fn qriter<R>(reader: R) -> QrIter<R>
+    where
+        R: Read,
+    {
+        QrIter::new(reader)
     }
-    Ok(())
+
+    #[derive(Debug)]
+    pub struct QrIter<R> {
+        buffer: [u8; 2048],
+        reader: BufReader<R>,
+    }
+
+    impl<R> QrIter<R>
+    where
+        R: Read,
+    {
+        fn new(reader: R) -> Self {
+            QrIter {
+                buffer: [0; 2048],
+                reader: BufReader::new(reader),
+            }
+        }
+    }
+
+    impl<R> Iterator for QrIter<R>
+    where
+        R: Read,
+    {
+        type Item = ImageBuffer<Luma<u8>, Vec<u8>>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            loop {
+                let len = match self.reader.read(&mut self.buffer) {
+                    Ok(0) => return None,
+                    Ok(len) => len,
+                    Err(e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
+                    Err(_) => return None,
+                };
+                let code = QrCode::new(&mut self.buffer[..len]).ok()?;
+                return Some(code.render::<Luma<u8>>().build());
+            }
+        }
+    }
 }
